cdc0480c43c78b05bb7ab308b18e4aae982b54c3
Piotr Gajcy <pgajcy@macrosystem.pl>
2023-02-07 11:56:03 +0100

    added "Kolędy" section

--- bin/stats.sh
+++ bin/stats.sh
@@ -6,7 +6,7 @@ echo -e '\\TabPositions{9cm}' >> ./tmp/songbook_2.tmp
 echo -e '{\\footnotesize' >> ./tmp/songbook_2.tmp
 echo -e '\\textbf{Wykonawca - Album  \\tab Liczba piosenek} \n\n' >> ./tmp/songbook_2.tmp
 for i in $(find ./src/songs/ -type f -name "*.tex"); do grep -oP 'rightline{\(\K[^\)}]+' $i | grep -v capo | grep -vi tonacja; done | sort | uniq -c | sed 's/-"Nieznany"//g' | awk '{first = $1; $1=""; print $0, " \\tab", first " \\newline"}' >> ./tmp/songbook_2.tmp
-echo -e '\\textbf{\nWykonawca \\tab Liczba piosenek} \n\n' >> ./tmp/songbook_2.tmp
+echo -e '\\newpage\\textbf{\nWykonawca \\tab Liczba piosenek} \n\n' >> ./tmp/songbook_2.tmp
 for i in $(find ./src/songs/ -type f -name "*.tex"); do grep -oP 'rightline{\(\K[^-]+' $i | grep -v capo | grep -vi tonacja; done | sort | uniq -c | sed 's/-"Nieznany"//g' | awk '{first = $1; $1=""; print $0, " \\tab", first " \\newline"}' >> ./tmp/songbook_2.tmp
 echo -e '\n }' >> ./tmp/songbook_2.tmp
 echo -e '\n\n \\end{document}' >> ./tmp/songbook_2.tmp
